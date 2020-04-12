52b2a9adb9d5b51ae0b9caaecc68f0e9197516f4
anshved <ansh.ved@sakec.ac.in>
2020-04-12 15:28:17 +0530

    smooth scroll done

--- js/main.js
+++ js/main.js
@@ -23,17 +23,39 @@ $(document).ready(function() {
 
     $(this).addClass('active');
 
-    var target = this.hash,
-        menu = target;
+    // var target = this.hash,
+    //     menu = target;
 
-    target = $(target);
-    $('html, body').stop().animate({
-      'scrollTop': target.offset().top - 80
-    }, 500, 'swing', function() {
-      window.location.hash = target.selector;
-      $(document).on("scroll", onScroll);
-    });
+    // target = $(target);
+    // $('html, body').stop().animate({
+    //   'scrollTop': target.offset().top - 80
+    // }, 500, 'swing', function() {
+    //   window.location.hash = target.selector;
+    //   $(document).on("scroll", onScroll);
+    // });
   });
+
+    // Add smooth scrolling on all links inside the navbar
+    $("#main-nav a").on('click', function(event) {
+      // Make sure this.hash has a value before overriding default behavior
+      if (this.hash !== "") {
+        // Prevent default anchor click behavior
+        event.preventDefault();
+  
+        // Store hash
+        var hash = this.hash;
+  
+        // Using jQuery's animate() method to add smooth page scroll
+        // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+        $('html, body').animate({
+          scrollTop: $(hash).offset().top - 80
+        }, 800, function(){
+     
+          // Add hash (#) to URL when done scrolling (default click behavior)
+          // window.location.hash = hash ;
+        });
+      }  // End if
+    });
 
 
   function onScroll(event) {
